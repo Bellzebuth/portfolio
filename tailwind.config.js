4ce739ac0918a16d162c37b55268cf617f04e91b
Etienne Tournier Rigaudy <e.tournier.rigaudy@gmail.com>
2025-01-30 16:48:48 +0100

    fix switch to dark mode

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,18 +1,17 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    './src/**/*.{html,js,jsx,ts,tsx}', // Ajoutez tous les fichiers où Tailwind doit chercher les classes
-    './index.html', // Si vous avez ce fichier
-    'node_modules/preline/dist/*.js', // Assurez-vous que Preline est également inclus
+    "./src/**/*.{html,js,jsx,ts,tsx}",
+    "./index.html",
+    "node_modules/preline/dist/*.js",
   ],
+  darkMode: "class",
   theme: {
     extend: {
       colors: {
-        'purple-hover': '#2e024d',
+        "purple-hover": "#2e024d",
       },
     },
   },
-  plugins: [
-    require('preline/plugin'), // Assurez-vous que le plugin Preline est bien chargé
-  ],
-}
+  plugins: [require("preline/plugin")],
+};
